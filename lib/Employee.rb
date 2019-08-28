6e0be8fa82192b4195164dee6380fe835c8ea43a
github username <github email address>
2019-08-28 12:18:40 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,36 @@
 class Employee
+    attr_accessor :name, :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
+
+    def manager_name
+        @manager.name
+    end
+
+    def tax_bracket
+        @@all.select{|x|
+            (x.salary - @salary <= 1000 && x.salary >= @salary) || (@salary - x.salary <= 1000 && @salary >= x.salary)
+        }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(num)
+        @@all.select{|x| x.salary > num}
+    end
+
+    def self.find_by_department(dept)
+        manager = Manager.all.find{|x| x.department == dept}
+        return manager.employees[0]
+    end
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,39 @@
 class Manager
+    attr_accessor :name, :department, :age
+
+    @@all = []
+
+    def initialize(name,department,age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+
+    def employees
+        Employee.all.select {|x| x.manager == self}
+    end
+
+    def hire_employee(name, salary)
+        Employee.new(name,salary,self)
+    end
+
+    def self.all_departments
+        dept = []
+        @@all.each{|x| dept << x.department}
+        return dept
+    end
+
+    def self.average_age
+        count = @@all.length.to_f
+        sum = 0.0
+        @@all.each{|x| sum+= x.age.to_f}
+        average = sum/count
+        return average
+    end
+
+    def self.all
+        @@all
+    end
 
 end
